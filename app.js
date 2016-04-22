e1a5b3778e3b2a901ec894b02f709b4020fa3fcf
Jeremy Perez <perezje43@gmail.com>
2016-04-22 08:55:13 -0700

    added css to html

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var times = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12AM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', 'TOTAL'];
 var locations = [];
 
+var newStore = document.getElementById('new-store');
+
 function StoreLocations(store, min, max, avgCookies) {
   this.store = store;
   this.minimumCustomers = min;
@@ -12,7 +14,7 @@ function StoreLocations(store, min, max, avgCookies) {
   };
   this.randomCookiesPerHour = function() {
     return Math.floor(this.randomVisitorsPerHour() * this.averageCookiesPerHour);
-  },
+  };
 
   this.cookies = [];
   this.customers = [];
@@ -46,17 +48,17 @@ function createHeader() {
     header.appendChild(th);
   }
 }
-createHeader();
 
 function createBody() {
   var bodyRow = document.getElementById('bodyRow');
+  bodyRow.innerHTML = '';
   for(i = 0; i < locations.length; i++) {
     var tr = document.createElement('tr');
     bodyRow.appendChild(tr);
     var td = document.createElement('td');
     td.textContent = locations[i].store;
     tr.appendChild(td);
-    for(x = 0; x < locations[i].cookies.length; x++) {
+    for(var x = 0; x < locations[i].cookies.length; x++) {
       var td2 = document.createElement('td');
       td2.textContent = locations[i].cookies[x];
       tr.appendChild(td2);
@@ -65,27 +67,47 @@ function createBody() {
     td3.textContent = locations[i].dailyTotal;
     tr.appendChild(td3);
   }
-  locations = [];
 }
-createBody();
-
-var newStore = document.getElementById('new-store');
 
 function handleNewStoreSubmit(event) {
   event.preventDefault();
   var newStoreName = event.target.storeName.value;
   var newMinimumCustomers = event.target.minimumCustomers.value;
   var newMaximumCustomers = event.target.maximumCustomers.value;
-  var newAverageCookies = event.target.averageCookies.value;
+  var newAverageCookiesPerHour = event.target.averageCookies.value;
 
-  var newStore = new StoreLocations(newStoreName, newMinimumCustomers, newMaximumCustomers, newAverageCookies);
+  var j = 1;
+
+  for(var x = 0; x < locations.length; x++) {
+    if(newStoreName === locations[x].store) {
+      locations[x].minimumCustomers = parseInt(newMinimumCustomers);
+      locations[x].maximumCustomers = parseInt(newMaximumCustomers);
+      locations[x].averageCookiesPerHour = parseInt(newAverageCookiesPerHour);
+      locations[x].cookies = [];
+      locations[x].customers = [];
+      locations[x].dailyTotal = 0;
+      locations[x].randomCookiesPerHour();
+      locations[x].createArrays();
+      createBody();
+    }else{
+      j++;
+    }
+  }
+  console.log(locations.length);
+  console.log(j);
+
+  if(j > locations.length) {
+    var newStore = new StoreLocations(newStoreName, newMinimumCustomers, newMaximumCustomers, newAverageCookiesPerHour);
+    createBody();
+  }
 
   event.target.storeName.value = null;
   event.target.minimumCustomers.value = null;
   event.target.maximumCustomers.value = null;
   event.target.averageCookies.value = null;
-
-  createBody();
-}
+};
 
 newStore.addEventListener('submit', handleNewStoreSubmit);
+
+createHeader();
+createBody();
